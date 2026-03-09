4ccb01cc7ee7be93f49059d6d0f30a0a5253de38
yk-graph <yokokura.tatsuya@gmail.com>
2026-03-09 15:23:01 -0700

    chore: add comments to jest.config.ts for clarity

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    verbose: true
+  preset: 'ts-jest', // Use ts-jest preset to enable TypeScript support
+  testEnvironment: 'node', // Set the test environment to Node.js
+  verbose: true, // Enable verbose output to show individual test results
 }
 
-export default config;
+export default config
