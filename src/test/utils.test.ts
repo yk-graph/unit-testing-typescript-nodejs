8f322223d60040a8b8d1a96e435d31c5620e3b9c
yk-graph <yokokura.tatsuya@gmail.com>
2026-03-09 19:52:19 -0700

    chore: update utils functions and tests for string manipulation and add Prettier configuration

--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -1,15 +1,36 @@
-import { toUpperCase } from '../app/utils'
+import { getStringInfo, toUpperCase } from "../app/utils"
 
-describe('Utils test suite', () => {
-  it('should return upper case', () => {
+describe("Utils test suite", () => {
+  it("should return upper case", () => {
     // Arrange
     const sut = toUpperCase
-    const expected = 'HELLO WORLD'
+    const expected = "HELLO WORLD"
 
     // Act
-    const actual = sut('hello world')
+    const actual = sut("hello world")
 
     // Assert
     expect(actual).toBe(expected)
+  })
+
+  it("should return info for valid string", () => {
+    const actual = getStringInfo("Hello world")
+
+    expect(actual.lowerCase).toBe("hello world")
+    expect(actual.extraInfo).toEqual({})
+
+    expect(actual.characters.length).toBe(11)
+    expect(actual.characters).toHaveLength(11)
+
+    expect(actual.characters).toEqual(["H", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"])
+    expect(actual.characters).toContain<string>("H")
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["H", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]),
+    )
+
+    expect(actual.extraInfo).not.toBe(undefined)
+    expect(actual.extraInfo).not.toBeUndefined()
+    expect(actual.extraInfo).toBeDefined()
+    expect(actual.extraInfo).toBeTruthy()
   })
 })
